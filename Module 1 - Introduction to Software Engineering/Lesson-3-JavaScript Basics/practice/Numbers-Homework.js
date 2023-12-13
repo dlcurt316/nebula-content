51e12e990d3815f39b00649091e29c93992f92a4
David Curtis <dlcurt316@gmail.com>
2023-12-13 12:16:10 -0500

    Numbers-Homework

---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/practice/Numbers-Homework.js
@@ -0,0 +1,91 @@
+//SUBMIT A LINK TO THE GITHUB REPOSITORY CONTAINING THIS ASSIGNMENT
+// Remember to console.log() to debug your code and check your solutions!
+
+
+// Declare a variable and assign it to a numeric value
+// Half that value and re-assign it to the previous variable 
+
+
+// Create two negative numbers, assign them to variables and subtract one from the other together 
+
+
+// Create three numbers, assign them to variables and multiply them together 
+
+
+// Create two even numbers, assign them to variables and divide one from the other 
+
+
+// Create two odd numbers, assign them to variables and find the remainder of dividing one by the other 
+
+
+// find the minimum value in a set of numbers: 10,20,30,40,50 
+
+
+// find the maximum value in a set of numbers: -1,-2,-3,-4,-5 
+
+
+// Create a variable and find it's remainder, if divided by 2
+
+
+// find the square root of 144 
+
+
+// find the value of 3 to the power of 7 
+
+
+// find the value of 73.2 rounded 
+
+
+// find the rounded-down value of 9.9 
+
+
+// find the absolute value of -1231
+
+const x = 16
+y = x / 2
+console.log(y= x / 2)
+
+let num1 = -7
+let num2 = -12
+num1 - num2
+console.log(num1 - num2)
+
+let num4 = 8
+let num5= 109
+let num6 = 15
+let num7 = num4 * num5 * num6
+console.log(num7)
+
+let num8 = 8
+let num9 = 2
+let num10 = num8 / num9
+console.log(num10)
+
+let num11 = 15
+let num12 = 5
+let num13 = num11 % num12
+console.log(num13)
+
+Math.min(10,20,30,40,50)
+console.log(Math.min(10,20,30,40,50))
+
+Math.max(-1,-2,-3,-4,-5)
+console.log(Math.max(-1,-2,-3,-4,-5))
+
+const n = 8 % 2
+console.log(n)
+
+Math.sqrt(144)
+console.log(Math.sqrt(144))
+
+Math.pow(3,7)
+console.log(Math.pow(3,7))
+
+Math.round(73.2)
+console.log(Math.round(73.2))
+
+Math.floor(9.9)
+console.log(Math.floor(9.9))
+
+Math.abs(-1231)
+console.log(Math.abs(-1231))
---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/practice/Numbers-Practice-1.js
@@ -0,0 +1,57 @@
+
+// Declare a variable and assign it to a numeric value
+// Double that value and re-assign it to the previous variable
+
+
+
+// Create two numbers, assign them to variables and add them together
+ 
+
+
+// Create three numbers, assign them to variables and subtract the smaller two from the largest
+
+
+
+// Create two even numbers, assign them to variables and divide one from the other
+
+
+
+// Create two odd numbers, assign them to variables and multiply one from the other
+
+
+
+// Create a variable and log it's remainder, if divided by 3, to the console 
+
+
+
+// Answer in plain text, what is modulo especially useful for?
+
+
+let num1 = 5
+num1 *= 2
+console.log(num1)
+
+const num2 = 50
+const num3 = 11
+const sum = num2 + num3
+console.log(sum)
+
+let num4 = 5
+let num5 = 10
+let num6 = 15
+const result = num6 - num5 - num4
+console.log(result)
+
+const num7 = 6
+const num8 = 2
+const result1 =  num7 / num8
+console.log(result1)
+
+const num9 = 7
+const num10 = 9
+const result2 = num9 * num10
+console.log(result2)
+
+const num11 = 20
+console.log(num11 % 3)
+
