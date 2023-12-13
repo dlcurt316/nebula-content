4048573f79f90d377afa2b30b7b69e59fb0d884e
David Curtis <dlcurt316@gmail.com>
2023-12-13 14:10:24 -0500

    Numbers-Homework1

---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/practice/Numbers-Practice-2.js
@@ -0,0 +1,58 @@
+
+// For these questions use the Math methods that JavaScript includes
+// All Math methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+
+// find the square root of 25
+
+
+// find the value of 3 to the power of 5
+
+
+// find the value of 7.4 rounded
+
+
+// find the rounded-up value of 7.4
+
+
+
+// find the absolute value of -13
+
+
+// find the minimum value in a set of numbers: 12,13,14,15,2,-1
+
+
+// find the maximum value in a set of numbers: 12,13,14,15,2,-1
+
+
+// create a random number 
+
+
+// Create a random number between 0-10
+
+Math.sqrt(25)
+console.log(Math.sqrt(25))
+
+Math.pow(3,5)
+console.log(Math.pow(3,5))
+
+Math.round(7.4)
+console.log(Math.round(7.4))
+
+Math.ceil(7.4)
+console.log(Math.ceil(7.4))
+
+Math.abs(-13)
+console.log(Math.abs(-13))
+
+Math.min(12,13,14,15,2,-1)
+console.log(Math.min(12,13,14,15,2,-1))
+
+Math.max(12,13,14,15,2,-1)
+console.log(Math.max(12,13,14,15,2,-1))
+
+Math.random()
+console.log(Math.random())
+
+Math.random() * 10
+console.log(Math.random() * 10)
