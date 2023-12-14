12ca6235b79ff3381df27e2011802484a5b87863
David Curtis <dlcurt316@gmail.com>
2023-12-14 11:30:11 -0500

    stringshomework1

---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/practice/stringshomework1.js
@@ -0,0 +1,41 @@
+const string1 = "hello"
+const string2 = "goodbye"
+const concatenatedString = string1 + string2
+console.log(concatenatedString)
+
+const greeting = "Hello Friend";
+console.log(greeting.slice(6,12))
+
+let exclamaition = "Hello"
+let pronoun = "I"
+let adverb = "also"
+let comparison = "like"
+let sport  = "football"
+const savedTemplateLiteral = `${exclamaition}, ${pronoun}, ${adverb}, ${comparison}, ${sport}`
+console.log(savedTemplateLiteral)
+
+const myName = "David Curtis"
+const initials = myName[0] + myName[6]
+console.log(initials)
+
+const greeting1 = "Hello World"
+console.log(greeting1[4])
+
+const coding = 'Coding-Time!'
+const index = coding.indexOf("-")
+console.log(index)
+
+const string = "tiger"
+const finalLetter = string[string.length - 1]
+console.log(finalLetter)
+
+const strings = "elephant"
+console.log(strings.slice(0,3))
+
+const sentence1 = "This-is-a-sentence"
+const sentence1Array = sentence1.split(" ")
+console.log(sentence1.split(" "))
+
+let one = 1;
+let two = 2;
+console.log("1" + "2")
